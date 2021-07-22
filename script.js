a4a11643eed07197ed2df855484447364169d5d1
TRISNA <77385311+Bicoding14@users.noreply.github.com>
2021-07-22 20:55:52 +0700

    komenter penanda baru

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function showModal (titleHtml, contentHtml, buttons) {
 
+    // create elements
     const modal =  document.createElement('div');
+    // console.log(modal);
     modal.classList.add('modal');
+    // console.log(modal);
 
     modal.innerHTML = 
     `<div class="modal__inner">
@@ -14,12 +17,13 @@ function showModal (titleHtml, contentHtml, buttons) {
         <div class="modal__content">${contentHtml}</div>
         <div class="modal__bottom"></div>
     </div>`;
-
+    
+    // console.log(buttons);
     for (const button of buttons) {
 
-        // console.log(button.label);
+        // console.log(button.triggerClose);
         const element = document.createElement('button');
-        
+        // console.log(element);
         element.setAttribute('type', 'button');
         element.classList.add('modal__button');
         element.textContent = button.label;
@@ -30,10 +34,13 @@ function showModal (titleHtml, contentHtml, buttons) {
                 document.body.removeChild(modal);
             }
 
-            button.onClick(modal);
+            button.onClick();
         });
         
+        // console.log(element);
+        // add button element
         modal.querySelector('.modal__bottom').appendChild(element);
+        console.log(modal);
 
         modal.querySelector('.modal__close').addEventListener ('click', () => {
             document.body.removeChild(modal);
@@ -47,11 +54,11 @@ showModal('Sample Modal Title','<p> I am the content of this modal </p>', [
 
     {
         label: "Go it!",
-        onClick: modal => {
+        triggerClose: true,
+        onClick: () => {
             console.log('The button was clicked!')
-        },
-        triggerClose: false
+        }
     }
 ]);
 
-console.log(showModal);
+// console.log(showModal);
